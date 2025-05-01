9d90b871bea02f9c4c7bbca0784e0b76df224952
Reza Yousefpour <R.A.Zeytoon@gmail.com>
2025-05-01 11:44:10 +0330

    Update quartz.config.ts

--- quartz.config.ts
+++ quartz.config.ts
@@ -12,10 +12,36 @@ const config: QuartzConfig = {
     baseUrl: "systophia.ir",   // بدون https:// و اسلش
 	defaultDateType: "modified", 
     theme: {
-      typography: { header: "Vazirmatn", body: "Vazirmatn", code: "IBM Plex Mono" },
+      fontOrigin: "googleFonts",
+      cdnCaching: true,
+      typography: {
+        header: "Vazirmatn",
+        body: "Vazirmatn",
+        code: "IBM Plex Mono",
+      },
       colors: {
-        lightMode: { secondary: "#3f8cff", tertiary: "#ffa726" },
-        darkMode:  { secondary: "#7b97aa", tertiary: "#ffa726" },
+        lightMode: {
+          light: "#faf8f8",
+          lightgray: "#e5e5e5",
+          gray: "#b8b8b8",
+          darkgray: "#4e4e4e",
+          dark: "#2b2b2b",
+          secondary: "#284b63",
+          tertiary: "#84a59d",
+          highlight: "rgba(143, 159, 169, 0.15)",
+          textHighlight: "#fff23688",
+        },
+        darkMode: {
+          light: "#161618",
+          lightgray: "#393639",
+          gray: "#646464",
+          darkgray: "#d4d4d4",
+          dark: "#ebebec",
+          secondary: "#7b97aa",
+          tertiary: "#84a59d",
+          highlight: "rgba(143, 159, 169, 0.15)",
+          textHighlight: "#b3aa0288",
+        },
       },
     },
     styleOverrides: ["./static/custom.scss"], // فایل RTL
